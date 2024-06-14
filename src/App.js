d03d392486acccc5616e18bd845a3440c62da3b6
SarvT <91484197+SarvT@users.noreply.github.com>
2024-06-14 10:17:35 +0530

    insights gain

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-// import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <SpeedInsights/>
     </div>
   );
 }
