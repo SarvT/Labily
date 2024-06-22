c7e5ff4e31e7d8152dc8c5d3cde015b260f75f83
SarvT <91484197+SarvT@users.noreply.github.com>
2024-06-22 18:51:33 +0530

    added responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,39 @@
-import React from "react";
-import "./style.css"
+import React, {useState} from "react";
+import "./style.css";
+import { useMediaQuery } from "react-responsive";
 
-function Navbar(){
-    return(
-        <>
-        <div>
-            <nav>
-                <div className="logo">LaBily</div>
-                <ul>
-                    <li>Services</li>
-                    <li>Contact</li>
-                    <li>About</li>
-                    <li>Donate</li>
-                    <li>Volunteer</li>
-                </ul>
-            </nav>
-        </div>
-        </>
-    )
+// function menuAnimation(ctx){
+//   ctx.classList.toggle("change")
+// }e
+
+function Navbar() {
+  const isMobile = useMediaQuery({ query: `(max-width:760px)` });
+  const [change, setChange] = useState(false);
+  return (
+    <>
+      <div>
+        <nav>
+          <div className="logo">LaBily</div>
+          <ul>
+            <li>Donate</li>
+            <li>Volunteer</li>
+            <li>Services</li>
+            <li>Contact</li>
+            <li>About</li>
+          </ul>
+          { isMobile ?
+            <div className={change ? `menu change` : `menu`} onClick={()=>{setChange(!change)}}>
+              <div className="bar1"></div>
+              <div className="bar2"></div>
+              <div className="bar3"></div>
+            </div>
+            :
+            <></>
+          }
+        </nav>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,63 +1,96 @@
 import React from "react";
 
-function Services(){
-    return(
-        <>
-        <div className="ser-main">
-
-            <h4 className="ser-head">We Offer</h4>
-            <div className="services">
-                {/* <p className="cnt-subhead">We care for the pets,<br />
+function Services() {
+  return (
+    <>
+      <div className="ser-main">
+        <h4 className="ser-head">We Offer</h4>
+        <div className="services">
+          {/* <p className="cnt-subhead">We care for the pets,<br />
                     Contact us for any help.
                 </p> */}
-                <div className="ser-cards">
-
-                <div className="cards">
-                    <div className="card">
-                        <div className="card-head"><i class="fa-solid fa-bowl-rice"></i> Pet food</div>
-                        <div className="card-desc">Premium, nutritious pet food for a healthy, happy life.
-Delicious and balanced meals tailored for your pet's needs.
-</div>
-                        <div className="card-cta">Know more...</div>
-                    </div>
-                    <div className="card" style={{backgroundColor:"white", color:"orange", border:"1px solid orange"}}>
-                        <div className="card-head"><i class="fa-solid fa-shield-heart"></i>Pet care</div>
-                        <div className="card-desc">Professional pet sitting and grooming services for your furry friends.
-Reliable pet sitting, walking, and grooming for happy, healthy pets.</div>
-                        <div className="card-cta">Know more...</div>
-                    </div>
+          <div className="ser-cards">
+            <div className="cards">
+              <div className="card">
+                <div className="card-head">
+                  <i class="fa-solid fa-bowl-rice"></i> Pet food
                 </div>
-                <div className="cards">
-                    <div className="card" style={{backgroundColor:"white", color:"orange", border:"1px solid orange"}}>
-                        <div className="card-head"><i class="fa-solid fa-camera-retro" ></i>Pet Shoot</div>
-                        <div className="card-desc">Capture precious moments with stunning pet photography sessions.
-Professional pet portraits to cherish your furry friend's memories.</div>
-                        <div className="card-cta">Know more...</div>
-                    </div>
-                    <div className="card">
-                        <div className="card-head"><i class="fa-solid fa-fish-fins"></i>Per Experts</div>
-                        <div className="card-desc">Expert advice and care for all your pet-related concerns.
-Certified professionals providing top-notch pet care and guidance.</div>
-                        <div className="card-cta">Know more...</div>
-                    </div>
+                <div className="card-desc">
+                  Premium, nutritious pet food for a healthy, happy life.
+                  Delicious and balanced meals tailored for your pet's needs.
                 </div>
-
+                <div className="card-cta">Know more...</div>
+              </div>
+              <div
+                className="card"
+                style={{
+                  backgroundColor: "white",
+                  color: "orange",
+                  border: "1px solid orange",
+                }}
+              >
+                <div className="card-head">
+                  <i class="fa-solid fa-shield-heart"></i>Pet care
                 </div>
-                <div className="ser-second">
-                    <h3>Pet care with love</h3>
-                    <p>We offer high-class care services and activities for your pets. No matter what accommodation type you choose, your pet will alwaуз receive premium meals, daily exercise and play time and constant care from our professional trained team of pet welfare staff</p>
-                <div className="ser-cnt">
-
-                    <input placeholder="Samuel Kinder" type="text" name="name" />
-                    <input placeholder="user@mail.com" type="email" name="email" />
-                    <textarea placeholder="Any message?" type="text" name="desc" />
-                    <button type="submit">Submit</button>
+                <div className="card-desc">
+                  Professional pet sitting and grooming services for your furry
+                  friends. Reliable pet sitting, walking, and grooming for
+                  happy, healthy pets.
                 </div>
-                </div>
+                <div className="card-cta">Know more...</div>
+              </div>
             </div>
+            <div className="cards">
+              <div
+                className="card"
+                style={{
+                  backgroundColor: "white",
+                  color: "orange",
+                  border: "1px solid orange",
+                }}
+              >
+                <div className="card-head">
+                  <i class="fa-solid fa-camera-retro"></i>Pet Shoot
                 </div>
-        </>
-    );
+                <div className="card-desc">
+                  Capture precious moments with stunning pet photography
+                  sessions. Professional pet portraits to cherish your furry
+                  friend's memories.
+                </div>
+                <div className="card-cta">Know more...</div>
+              </div>
+              <div className="card">
+                <div className="card-head">
+                  <i class="fa-solid fa-fish-fins"></i>Per Experts
+                </div>
+                <div className="card-desc">
+                  Expert advice and care for all your pet-related concerns.
+                  Certified professionals providing top-notch pet care and
+                  guidance.
+                </div>
+                <div className="card-cta">Know more...</div>
+              </div>
+            </div>
+          </div>
+          <div className="ser-second">
+            <h3>Pet care with love</h3>
+            <p>
+              We offer high-class care services and activities for your pets. No
+              matter what accommodation type you choose, your pet will alwaуз
+              receive premium meals, daily exercise and play time and constant
+              care from our professional trained team of pet welfare staff
+            </p>
+            <div className="ser-cnt">
+              <input placeholder="Samuel Kinder" type="text" name="name" />
+              <input placeholder="user@mail.com" type="email" name="email" />
+              <textarea placeholder="Any message?" type="text" name="desc" />
+              <button type="submit">Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Services;
