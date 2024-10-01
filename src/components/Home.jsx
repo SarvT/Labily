4cc586c151a28ec21fc6d3f87c819ed18ceb394b
SarvT <91484197+SarvT@users.noreply.github.com>
2024-10-01 17:36:56 +0530

    reponsive layout fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,34 +5,35 @@ function Home() {
   const hoverAnim = " hover:drop-shadow-2xl duration-300";
 
   return (
-    <>
-      <div className="home-page">
-        <div className="main">
-          <div className="bg"></div>
-        </div>
+    <div className="home-page">
+      <div className="main">
+        <div className="bg"></div>
+      </div>
 
-        <div className="heading-elements">
-          <div className="heading">
-            <div className="main-heading">
-              <p>Give them</p>
-              <p>The care</p>
-              <p>they deserve.</p>
-              <p className="head-subline">We are just a click away.</p>
-            </div>
-            <img className="head-img" src={img1} alt="Image" />
-            <div className="btn-heading ">
-              <button
-                className={`ctn-btn ${hoverAnim}`}
-                style={{ backgroundColor: "orange", color: "white" }}
-              >
-                Our Services
-              </button>
-              <button className={`work-btn shadow-orange-400 hoshadow-xl`}>Contact Us</button>
-            </div>
+      <div className="heading-elements">
+        <div className="heading">
+          <div className="main-heading">
+            <p>Give them</p>
+            <p>The care</p>
+            <p>they deserve.</p>
+            <p className="head-subline">We are just a click away.</p>
           </div>
+          <img className="head-img" src={img1} alt="Image" />
+        </div>
+        <div className="btn-heading">
+          <button
+            className={`ctn-btn ${hoverAnim}`}
+            style={{ backgroundColor: "orange", color: "white" }}
+          >
+            Our Services
+          </button>
+          <button className={`work-btn shadow-orange-400 hoshadow-xl`}>
+            Contact Us
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
+
 export default Home;
